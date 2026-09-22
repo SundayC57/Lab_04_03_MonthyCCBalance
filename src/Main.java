ae9c2f8320d417e8a89dbadc9a2d535057e60580
SundayC57 <sundaycs@mail.uc.edu>
2026-09-21 22:22:07 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+int startingBalance = 5000;
+double interestRate = 0.17;
+double interestMonth1 = 0;
+double interestMonth2 = 0;
+double month1Cost = 0;
+double month2Cost = 0;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+interestMonth1 = interestRate * startingBalance;
+month1Cost = startingBalance + interestMonth1;
+interestMonth2 = interestRate * month1Cost;
+month2Cost = month1Cost + interestMonth2;
+
+System.out.println("The balance after one month will be " + month1Cost + " and then after two months it will be " + month2Cost);
+
+
 }
